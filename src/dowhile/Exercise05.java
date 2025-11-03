dba3187e7e2db282aab82e9bffbe04f5b021867d
nicms06 <nicolas.mingorance@iesnervion.es.com>
2025-11-03 09:02:02 +0100

    Ejercicio 5 de do-While realizado

--- src/dowhile/Exercise05.java
+++ src/dowhile/Exercise05.java
@@ -7,6 +7,7 @@
 package dowhile;
 
 import java.util.Scanner;
+
 import java.util.Random;
 
 public class Exercise05 {
@@ -20,46 +21,38 @@ public class Exercise05 {
 		Random rand = new Random();
 		
 		//Declare the variables
-		int num;
-		int computerNum;
+		int max = 101;
+		int min = 0;
+		int randNum;
 		int option;
 		
-		//Ask the user for the number
-		System.out.println("Please, input a number (1-100): ");
-		num = sc.nextInt();
-		
-		//The number must be between 1 and 100
-		while (num < 1 || num > 100) {
-			System.out.println("ERROR: the number must be between 1 and 100: ");
-			num = sc.nextInt();
-		}
-		
-		computerNum = (int) (Math.random() * 100);
-		System.out.println(computerNum);
-		
 		do {
+			//Show a random number
+			randNum = rand.nextInt(min, max);
+			System.out.println(randNum);
 			
-			//Ask the user if the number is bigger or smaller
-			System.out.println("The number is bigger or smaller? (1= bigger, 2= smaller): ");
+			//Ask the user if is smaller or bigger
+			System.out.println("Bigger = 1 | Smaller = 2 | Correct = 3");
 			option = sc.nextInt();
-				
-			while (option > 2 || option <1) {
-				System.out.println("ERROR, try again: ");
-				option = sc.nextInt();
-				}
-			if (option == 1) {
-				computerNum = rand.nextInt(computerNum, 101);
-				System.out.println(computerNum);
-				}
-			else {
-				computerNum = rand.nextInt(1, computerNum+1);
-				System.out.println(computerNum);
-			}
-		}
-		while (computerNum != num);
 			
-		System.out.println("Correct");
-
+			//The number must be 1 or 2
+			while (option != 1 && option != 2 && option != 3) {
+				System.out.println("ERROR, the number must be 1, 2 or 3. Please, try again: ");
+				option = sc.nextInt();
+			}
+			
+			if (option == 1) {
+				min = randNum + 1;
+			}
+			else if (option == 2){
+				max = randNum - 1;
+			}
+			
+		}
+		while (option != 3);
+		
+		//Show a message of correct
+		System.out.println("I've guessed your number!!!");
 		
 		//Close the Scanner
 		sc.close();
